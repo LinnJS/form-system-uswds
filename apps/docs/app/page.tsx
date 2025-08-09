9d872c62bd2bc88ff1e3dc0de736ac57ef5de409
Justin Linn <justin@linnjs.com>
2025-08-08 22:27:02 -0400

    feat(docs): Convert docs to tailwind

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@repo/ui/button";
 import Image, { type ImageProps } from "next/image";
-import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -12,18 +11,18 @@ const ThemeImage = (props: Props) => {
 
   return (
     <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
+      <Image {...rest} src={srcLight} className="block dark:hidden" />
+      <Image {...rest} src={srcDark} className="hidden dark:block" />
     </>
   );
 };
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-20 gap-16 font-sans">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ThemeImage
-          className={styles.logo}
+          className="dark:invert"
           srcLight="turborepo-dark.svg"
           srcDark="turborepo-light.svg"
           alt="Turborepo logo"
@@ -31,22 +30,25 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-mono">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/5 dark:bg-white/10 px-1 py-0.5 rounded font-semibold">
+              apps/docs/app/page.tsx
+            </code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
-        <div className={styles.ctas}>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className={styles.primary}
+            className="rounded-full border border-solid border-transparent bg-foreground text-background flex items-center justify-center gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 hover:bg-gray-800 dark:hover:bg-gray-300 transition-colors"
             href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className={styles.logo}
+              className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
@@ -58,30 +60,47 @@ export default function Home() {
             href="https://turborepo.com/docs?utm_source"
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-green-500"
+            className="rounded-full border border-solid border-black/10 dark:border-white/20 flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 hover:bg-gray-100 dark:hover:bg-gray-900 hover:border-transparent transition-colors"
           >
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
+        <Button
+          appName="docs"
+          className="rounded-full border border-solid border-black/10 dark:border-white/20 flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 hover:bg-gray-100 dark:hover:bg-gray-900 hover:border-transparent transition-colors"
+        >
           Open alert
         </Button>
       </main>
-      <footer className={styles.footer}>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center font-sans">
         <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
           Examples
         </a>
         <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://turborepo.com?utm_source=create-turbo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to turborepo.com →
         </a>
       </footer>
